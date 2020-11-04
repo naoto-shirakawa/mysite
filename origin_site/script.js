584baee8242c11862091d83d1bcee3ba56793cf1
naoto_shirakawa <gtr10721@gmail.com>
2020-11-05 00:22:35 +0900

    Create script.js

--- origin_site/script.js
+++ origin_site/script.js
@@ -7,3 +7,20 @@ $(function() {
     });
 })
 
+$('#prof-title').hover(
+    function() {
+        $('#prof-text').css('display', 'block').show();
+    },
+    function() {
+        $('#prof-text').css('display', 'none');
+    }
+);
+
+$('#msg-title').hover(
+    function() {
+        $('#msg-text').css('display', 'block').show();
+    },
+    function() {
+        $('#msg-text').css('display', 'none');
+    }
+);
